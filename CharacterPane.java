c0f3eaaebfe49df6425f432805d4dc9a9261caa2
EthanForsha <forshaer@jacks.sfasu.edu>
2022-11-10 15:03:50 -0600

    Removed unnecessary imports.

--- CharacterPane.java
+++ CharacterPane.java
@@ -3,7 +3,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 public class CharacterPane extends Pane {
